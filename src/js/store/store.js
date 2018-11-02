a4d19c3585cc60c897a1357cda5956c4db49c002
O. Josué <yashiro@Os-MacBook-Pro.fios-router.home>
2018-11-02 15:09:42 -0400

    added services view page

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -36,7 +36,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide1,
-					title: "My Bitch 1",
+					title: "My Product 1",
 					description:
 						"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
 				},
@@ -44,7 +44,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide2,
-					title: "My Bitch 2",
+					title: "My Product 2",
 					description:
 						"survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset"
 				},
@@ -52,7 +52,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide3,
-					title: "My Bitch 3",
+					title: "My Product 3",
 					description:
 						"sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 				},
@@ -60,7 +60,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide1,
-					title: "My Bitch 4",
+					title: "My Product 4",
 					description:
 						"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha"
 				},
@@ -68,7 +68,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide2,
-					title: "My Bitch 5",
+					title: "My Product 5",
 					description:
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
 				},
@@ -76,7 +76,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide3,
-					title: "My Bitch 6",
+					title: "My Product 6",
 					description:
 						"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
 				},
@@ -84,7 +84,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide1,
-					title: "My Bitch 7",
+					title: "My Product 7",
 					description:
 						'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. '
 				},
@@ -92,7 +92,7 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide2,
-					title: "My Bitch 8",
+					title: "My Product 8",
 					description:
 						"This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,"
 				},
@@ -100,11 +100,86 @@ const getState = scope => {
 					type: "product",
 					link: "products",
 					image: slide3,
-					title: "My Bitch 9",
+					title: "My Product 9",
 					description:
 						'"Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
 				}
 			],
+			services: [
+				{
+					type: "service",
+					link: "services",
+					image: slide1,
+					title: "Hair Service 1",
+					description:
+						"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide2,
+					title: "Hair Service 2",
+					description:
+						"survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset"
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide3,
+					title: "Hair Service 3",
+					description:
+						"sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide1,
+					title: "Hair Service 4",
+					description:
+						"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha"
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide2,
+					title: "Hair Service 5",
+					description:
+						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide3,
+					title: "Hair Service 6",
+					description:
+						"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide1,
+					title: "Hair Service 7",
+					description:
+						'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. '
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide2,
+					title: "Hair Service 8",
+					description:
+						"This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,"
+				},
+				{
+					type: "service",
+					link: "services",
+					image: slide3,
+					title: "Hair Service 9",
+					description:
+						'"Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+				}
+			],
+
 			featured: [
 				{
 					type: "Artist",
--- src/js/views/services.jsx
+++ src/js/views/services.jsx
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { Card } from "../component/card.jsx";
+import { Context } from "../store/appContext.jsx";
 
 //create your first component
 export default class Services extends Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Services Page</h1>
+			<div className="card-columns container">
+				<Context.Consumer>
+					{({ store }) => {
+						return (
+							<div>
+								{store.services.map((product, index) => {
+									return <Card key={index} item={product} />;
+								})}
+							</div>
+						);
+					}}
+				</Context.Consumer>
 			</div>
 		);
 	}
