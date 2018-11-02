7b106320714e2393d53f77a0cea4849d9ed83d51
O. Josué <yashiro@Os-MacBook-Pro.fios-router.home>
2018-11-02 14:34:28 -0400

    updated card for products and services

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from "react";
+import slide1 from "../../img/slide1.jpg";
+import slide2 from "../../img/slide2.jpg";
+import slide3 from "../../img/slide3.jpg";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class Card extends Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
+		var cardItem = this.props.item;
 		return (
 			<div className="card p-3">
-				<img className="card-img-top" src="..." alt="Card image cap" />
+				<img
+					className="card-img-top"
+					src={cardItem.image}
+					alt="Card image cap"
+				/>
 				<blockquote className="blockquote mb-0 card-body">
-					<h5 className="card-title">Card title</h5>
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-						Integer posuere erat a ante.
-					</p>
+					<h5 className="card-title">{cardItem.title}</h5>
+					<p>{cardItem.description}</p>
 					<footer className="blockquote-footer">
 						<small className="text-muted">
-							Someone famous in{" "}
-							<cite title="Source Title">Source Title</cite>
+							<Link to={cardItem.link}>Someone famous in </Link>
 						</small>
 					</footer>
 				</blockquote>
@@ -22,3 +32,6 @@ export class Card extends Component {
 		);
 	}
 }
+Card.propTypes = {
+	item: PropTypes.object
+};
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -33,22 +33,76 @@ const getState = scope => {
 
 			products: [
 				{
+					type: "product",
+					link: "products",
 					image: slide1,
-					title: "My Bitch 3",
-					content:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
-				},
-				{
-					image: slide2,
-					title: "My Bitch 3",
-					content:
-						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
-				},
-				{
-					image: slide3,
 					title: "My Bitch 1",
-					content:
+					description:
+						"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old."
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide2,
+					title: "My Bitch 2",
+					description:
+						"survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset"
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide3,
+					title: "My Bitch 3",
+					description:
+						"sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide1,
+					title: "My Bitch 4",
+					description:
+						"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It ha"
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide2,
+					title: "My Bitch 5",
+					description:
 						"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide3,
+					title: "My Bitch 6",
+					description:
+						"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide1,
+					title: "My Bitch 7",
+					description:
+						'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. '
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide2,
+					title: "My Bitch 8",
+					description:
+						"This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,"
+				},
+				{
+					type: "product",
+					link: "products",
+					image: slide3,
+					title: "My Bitch 9",
+					description:
+						'"Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
 				}
 			],
 			featured: [
