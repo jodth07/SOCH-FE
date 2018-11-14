cacb9dbc8322b42d21d6ad3fdb413ef86ad653a9
Guensie Grecy <guensie@gmail.com>
2018-11-14 23:38:15 +0000

    first draft artist page

--- src/js/views/artist.jsx
+++ src/js/views/artist.jsx
@@ -6,6 +6,43 @@ export default class ArtistPage extends Component {
 		return (
 			<div className="text-center mt-5">
 				<h1>Artists Page</h1>
+				<div
+					className="jumbotron jumbotron-fluid shorten"
+					style={{ backgroundColor: "black" }}>
+					<div className="container">
+						<h4 className="float-right" style={{ color: "white" }}>
+							I am a skilled stylist who specializes in wavy and
+							curly natural hair.
+						</h4>
+						<h2 className="display-4 left">
+							<img
+								src="https://images.pexels.com/photos/1405982/pexels-photo-1405982.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350"
+								width="350"
+								height="250"
+								className="rounded-circle"
+							/>
+						</h2>
+						<p className="lead artist_name">Janet Jones</p>
+					</div>
+				</div>
+				<div>
+					<p className="artistServices">
+						<h1> My Services </h1>
+						<ul className="list-group">
+							<li className="list-group-item">
+								Brazilian Keratin Treatment
+							</li>
+							<li className="list-group-item">
+								Wash and Silk press
+							</li>
+							<li className="list-group-item">
+								Single Plat braids
+							</li>
+							<li className="list-group-item">Formal Updo</li>
+							<li className="list-group-item">Beach waves</li>
+						</ul>
+					</p>
+				</div>
 			</div>
 		);
 	}
